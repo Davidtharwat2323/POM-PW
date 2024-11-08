e29e8bbbf7b36c5f1f7020fb4243bd98a75ba0be
Tharwat <dtharwat@deloitte.com>
2024-11-08 11:51:23 +0200

    Playwright POM 1

--- tests/8.UsePageObject.spec.ts
+++ tests/8.UsePageObject.spec.ts
@@ -2,7 +2,7 @@ import {test , expect} from '@playwright/test'
 import {NavigationPage} from '../Page-objects/NavigationPage'
 
 test.beforeEach("open URL", async ({ page }) => {
-    await page.goto("http://localhost:8888/");
+    await page.goto("http://localhost:4401/");
 })
 
 test("Navigate to Form Page" , async  ({ page }) => {
